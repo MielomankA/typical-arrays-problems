1f82dc9d1ab93d26ebb6df78df1a148f1ef7f240
MielomankA <mielomanka@mail.ru>
2021-08-22 02:58:43 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
+exports.min = function min(array) {
+    if (!array || array.length === 0) {
+        return 0;
+    }
 
-exports.min = function min (array) {
-  return 0;
-}
+    let result = Math.min(...array);
+    return result;
+};
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (!array || array.length === 0) {
+        return 0;
+    }
 
-exports.avg = function avg (array) {
-  return 0;
-}
+    let result = Math.max(...array);
+    return result;
+};
+
+exports.avg = function avg(array) {
+    if (!array || array.length === 0) {
+        return 0;
+    }
+
+    const numbers = [...array];
+    let totalSum = 0;
+
+    for (let i in numbers) {
+        totalSum += numbers[i];
+    }
+
+    let numsCnt = numbers.length;
+    if (!numsCnt) return 0;
+
+    return totalSum / numsCnt;
+};
